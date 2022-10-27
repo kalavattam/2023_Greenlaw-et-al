be00b5c5cf135ce6a87dd87a0df55f06b463f39f
Kris Alavattam <kalavatt@bs-16688.fhcrc.org>
2022-10-27 09:37:08 -0700

    Addition of submission scripts from GMAP and Trinity work on ~2022-1021

---
+++ results/2022-1021/submit-gmap_chrVII.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --error=./%J.err.txt
+#SBATCH --output=./%J.out.txt
+
+ml GMAP-GSNAP/2018-07-04-foss-2018b
+ml SAMtools/1.16.1-GCC-11.2.0
+
+d_Kris="${HOME}/tsukiyamalab/Kris"
+d_proj="2022_transcriptome-construction/results"
+d_exp="2022-1021/trinity_trin4s_5781_Q_IP_sorted.chrVII"
+fasta="Trinity-GG.fasta"
+
+gmap \
+	-d "sacCer3" \
+	-D "${HOME}/genomes/sacCer3/GMAP" \
+    -A "${d_Kris}/${d_proj}/${d_exp}/${fasta}" \
+    --format="gff3_gene" "samse" \
+    	> "${d_Kris}/${d_proj}/${d_exp}/${fasta%.fasta}.GMAP.gff"
+
