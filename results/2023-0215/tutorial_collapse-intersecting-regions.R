743db1646b6ce083dad90933b45019e68ed3f93c
Kris Alavattam <kalavatt@bs-16688.fhcrc.org>
2023-04-11 15:47:26 -0700

    Updating logic/explanations for the collapsing/merging work

--- results/2023-0215/tutorial_collapse-intersecting-regions.R
+++ results/2023-0215/tutorial_collapse-intersecting-regions.R
@@ -6,6 +6,8 @@
 
 #  See stackoverflow.com/questions/16957293/collapse-intersecting-regions
 
+library(plyr)
+
 
 d <- data.frame(
     chrom = c(1, 1, 1, 14, 16, 16), 
@@ -15,52 +17,58 @@ d <- data.frame(
 )
 
 
-#  Ignore chromosome ----------------------------------------------------------
-#  Make sure the data is sorted
-d <- d[order(d$start), ]
-
-#  Check if a record should be linked with the previous
-d$previous_stop <- c(NA, d$stop[-nrow(d)])
-d$previous_stop <- cummax(ifelse(is.na(d$previous_stop), 0, d$previous_stop))
-d$new_group <- is.na(d$previous_stop) | d$start >= d$previous_stop
-
-#  The number of the current group of records is the number of times we have
-#+ switched to a new group
-d$group <- cumsum(d$new_group)
-
-#  We can now aggregate the data
-plyr::ddply(
-    d,
-    "group",
-    summarize, 
-    start = min(start),
-    stop = max(stop),
-    name = paste(name, collapse = ",")
-)
+# #  Ignore chromosome ----------------------------------------------------------
+# #  Make sure the data is sorted
+# d <- d[order(d$start), ]
+# 
+# #  Check if a record should be linked with the previous
+# d$previous_stop <- c(NA, d$stop[-nrow(d)])
+# d$previous_stop <- cummax(ifelse(is.na(d$previous_stop), 0, d$previous_stop))
+# d$new_group <- is.na(d$previous_stop) | d$start >= d$previous_stop
+# 
+# #  The number of the current group of records is the number of times we have
+# #+ switched to a new group
+# d$group <- cumsum(d$new_group)
+# 
+# #  We can now aggregate the data
+# plyr::ddply(
+#     d,
+#     "group",
+#     summarize, 
+#     start = min(start),
+#     stop = max(stop),
+#     name = paste(name, collapse = ",")
+# )
 
 
 #  Don't ignore chromosome ----------------------------------------------------
-#  But this ignores the chrom column: To account for it, you can do the same
-#+ thing for each chromosome, separately.
+#  But this ignores the chrom column: To account for it, do the same thing for
+#+ each chromosome separately
 d <- d[order(d$chrom, d$start), ]
 d <- plyr::ddply(
     d,
     "chrom",
     function(u) { 
+        #  Check if a record should be linked with the previous record
         x <- c(NA, u$stop[-nrow(u)])
         y <- ifelse(is.na(x), 0, x)
         y <- cummax(y)
         y[is.na(x)] <- NA
         u$previous_stop <- y
+        
         return(u)
     }
 )
+
+#  Identify new groups based on 'previous_stop'
 d$new_group <- is.na(d$previous_stop) | d$start >= d$previous_stop
 d$group <- cumsum(d$new_group)
+
+#  Aggregate the data
 plyr::ddply(
     d,
     .(chrom, group),
-    summarize, 
+    plyr::summarize, 
     start = min(start),
     stop = max(stop),
     name = paste(name, collapse = ",")
